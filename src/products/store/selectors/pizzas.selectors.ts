f80b8a94b0401b3a723e99a59a443f50b13f5c41
Kunal Thorat <kunalthorat@gmail.com>
2017-12-31 20:53:44 -0500

    Adding toppings with multiple reducers/selectors/actions

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -3,9 +3,12 @@ import { createSelector } from '@ngrx/store';
 import * as fromRoot from '../../../app/store';
 import * as fromFeature from '../reducers';
 import * as fromPizzas from '../reducers/pizzas.reducer';
+import * as fromToppings from './toppings.selectors';
+
 import { getPizzaEntities } from '../reducers/pizzas.reducer';
 
 import { Pizza } from '../../models/pizza.model';
+import { getSelectedToppings } from 'src/products/store/reducers/toppings.reducer';
 
 // pizzas state
 export const getPizzaState = createSelector(
@@ -23,6 +26,16 @@ export const getSelectedPizza = createSelector(
   fromRoot.getRouterState,
   (entities, router): Pizza => {
     return router.state && entities[router.state.params.pizzaId];
+  },
+);
+
+export const getPizzaVisualised = createSelector(
+  getSelectedPizza,
+  fromToppings.getToppingEntities,
+  fromToppings.getSelectedToppings,
+  (pizza, toppingEntities, selectedToppings) => {
+    const toppings = selectedToppings.map(id => toppingEntities[id]);
+    return { ...pizza, toppings };
   },
 );
 
